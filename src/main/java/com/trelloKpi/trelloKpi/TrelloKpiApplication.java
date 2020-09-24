8f0d677fad30b7d5b725a91a6602c26011876156
Yarden.Ingber <yarden.ingber@applitools.com>
2020-09-24 14:25:17 +0300

    first commit

---
+++ src/main/java/com/trelloKpi/trelloKpi/TrelloKpiApplication.java
@@ -0,0 +1,13 @@
+package com.trelloKpi.trelloKpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloKpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloKpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trelloKpi/trelloKpi/TrelloKpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trelloKpi.trelloKpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloKpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
