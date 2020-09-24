5e4df363e80a82dfedfe25c83c5c0eed00eceacb
Yarden.Ingber <yarden.ingber@applitools.com>
2020-09-24 19:32:19 +0300

    add variables

--- src/main/java/com/trelloKpi/trelloKpi/DatabaseConfig.java
+++ src/main/java/com/trelloKpi/trelloKpi/DatabaseConfig.java
@@ -1,31 +1,31 @@
 package com.trelloKpi.trelloKpi;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
 
-import javax.sql.DataSource;
-
-@Configuration
+//@Configuration
 public class DatabaseConfig {
 
-    @Value(("${spring.datasource.driverClassName}"))
-    private String driver;
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        String uri = System.getenv("DATABASE_URL");
-        String url = "jdbc:postgresql://" + uri.split("@")[1];
-        String username = uri.split("@")[0].replace("postgres://", "").split(":")[0];
-        String password = uri.split("@")[0].replace("postgres://", "").split(":")[1];
-        config.setJdbcUrl(url);
-        config.setDriverClassName(driver);
-        config.setUsername(username);
-        config.setPassword(password);
-        return new HikariDataSource(config);
-    }
+//    @Value(("${spring.datasource.driverClassName}"))
+//    private String driver;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        HikariConfig config = new HikariConfig();
+//        String uri = System.getenv("DATABASE_URL");
+//        String url = "jdbc:postgresql://" + uri.split("@")[1];
+//        String username = uri.split("@")[0].replace("postgres://", "").split(":")[0];
+//        String password = uri.split("@")[0].replace("postgres://", "").split(":")[1];
+//        config.setJdbcUrl(url);
+//        config.setDriverClassName(driver);
+//        config.setUsername(username);
+//        config.setPassword(password);
+//        return new HikariDataSource(config);
+//    }
 
 }
